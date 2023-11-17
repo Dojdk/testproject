7660a0b8db5fbb2266ddd1b55defccd350e7a604
Ibrahim <ibrahim@Ibrahims-MacBook-Pro.local>
2023-11-17 18:59:41 +0400

    Finished test project

--- lib/widgets/followbutton.dart
+++ lib/widgets/followbutton.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'package:provider/provider.dart';
-import 'package:testproject/widgets/animationwrapper.dart';
 
 import '../providers/user.dart';
 
@@ -17,34 +16,32 @@ class _FollowButtonState extends State<FollowButton> {
   @override
   Widget build(BuildContext context) {
     late final bool isFollowed = userProvider.userIsFollowed(widget.id);
-    return AnimatedWrapper(
-      child: GestureDetector(
-        onTap: () {
-          userProvider.toggleFollow(widget.id);
-        },
-        child: AnimatedContainer(
-          duration: const Duration(milliseconds: 300),
-          width: isFollowed ? 40 : 120,
-          height: 40,
-          decoration: BoxDecoration(
-            color: isFollowed ? Colors.red : Colors.transparent,
-            borderRadius: BorderRadius.circular(25),
-            border: isFollowed ? null : Border.all(color: Colors.red, width: 2),
-          ),
-          child: isFollowed
-              ? const Icon(
-                  Icons.account_circle_outlined,
-                  size: 30,
-                  color: Colors.white,
-                )
-              : const Center(
-                  child: Text(
-                    'FOLLOW',
-                    style: TextStyle(
-                        color: Colors.red, fontWeight: FontWeight.bold),
-                  ),
-                ),
+    return GestureDetector(
+      onTap: () {
+        userProvider.toggleFollow(widget.id);
+      },
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        width: isFollowed ? 40 : 120,
+        height: 40,
+        decoration: BoxDecoration(
+          color: isFollowed ? Colors.red : Colors.transparent,
+          borderRadius: BorderRadius.circular(25),
+          border: isFollowed ? null : Border.all(color: Colors.red, width: 2),
         ),
+        child: isFollowed
+            ? const Icon(
+                Icons.account_circle_outlined,
+                size: 30,
+                color: Colors.white,
+              )
+            : const Center(
+                child: Text(
+                  'FOLLOW',
+                  style:
+                      TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
+                ),
+              ),
       ),
     );
   }
